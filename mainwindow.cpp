a124def62700f1e26b0b1f8a683f9c32501c4886
stannyst <drahlovaleksandr@gmail.com>
2024-10-20 19:42:12 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,4 +14,19 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+void MainWindow::num()
+{   
+    QPushButton *qpb = (QPushButton*)sender();
+    QString digits;
+    if ((ui->Result->text().contains('.')) && (qpb->text() == "0"))
+    {
+        digits = ui->Result->text() + qpb->text();
+    }
+    else
+    {   
+        double numbers = (ui->Result->text() + qpb->text()).toDouble();
+        digits = QString::number(numbers, 'g', 15);
+    }
 
+    ui->Result->setText(digits);
+}
